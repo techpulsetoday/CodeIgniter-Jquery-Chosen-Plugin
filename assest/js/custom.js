67be72fda35e2e1eac71fe33092dc2d3f98eded0
techpulsetoday <techpulsetoday@gmail.com>
2017-07-09 21:38:37 +0530

    Modifying Custom JS file

--- assest/js/custom.js
+++ assest/js/custom.js
@@ -2,10 +2,10 @@ jQuery(document).ready(function($) {
 	// body...
 	$("select").chosen(
 		{
-			max_selected_options: 5,
-			no_results_text: "Oops, nothing found!",
-			allow_single_deselect: true,
-			width: "20%",
+			// max_selected_options: 5,
+			// no_results_text: "Oops, nothing found!",
+			// allow_single_deselect: true,
+			// width: "20%",
 		}
 		);
 });
